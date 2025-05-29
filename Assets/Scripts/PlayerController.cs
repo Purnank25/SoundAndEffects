05aaa79e5186586a578e202e583ea325721b3c4b
Purnank25 <purnankexcel1@gmail.com>
2025-05-29 17:01:13 +0530

    added death state and proper values were assigned to player for better physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,27 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody playerRb;
-    private bool isOnGround = true;
-    private float jumpForce = 10.0f;
+    public bool isOnGround = true;
+    public float jumpForce = 10.0f;
     public bool gameOver ;
+    public float gravityModifier = 1.0f;
+    private Animator playerAnim;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-     
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround )
         {
             playerRb.AddForce (Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -32,6 +37,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
