a23f89a33a82cf56ad1316440354974f7ccbeeef
Purnank25 <purnankexcel1@gmail.com>
2025-05-29 15:32:55 +0530

    Destroyed obstacles after a boundary

--- Assets/Scripts/SpwanManager.cs
+++ Assets/Scripts/SpwanManager.cs
@@ -6,19 +6,26 @@ public class SpwanManager : MonoBehaviour
     private Vector3 spwanPos = new Vector3(25,0,0);
     private float startDelay = 1;
     private float repeatRate = 1;
+    private PlayerController playerControllerScript;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("SpwanObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
-            }
+
+    }
     void SpwanObstacle()
     {
-        Instantiate(obstaclePrefab, spwanPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spwanPos, obstaclePrefab.transform.rotation);
+        }
 
     }
 }
